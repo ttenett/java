49c43f56a33fe6b00406ccf39adb0d81277cdf6e
ttenett <yeonjeongsdev@gmail.com>
2025-02-03 15:51:28 +0900

    feat: api 호출 - postman 활용

--- java/practice/weather/src/main/java/com/zerobase/weather/controller/DiaryController.java
+++ java/practice/weather/src/main/java/com/zerobase/weather/controller/DiaryController.java
@@ -3,14 +3,11 @@ package com.zerobase.weather.controller;
 import com.zerobase.weather.service.DiaryService;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
-@Controller
+@RestController
 public class DiaryController {
     // 받은 값들을 서비스에 전달해야 함.
     private final DiaryService diaryService;
--- java/practice/weather/src/main/java/com/zerobase/weather/service/DiaryService.java
+++ java/practice/weather/src/main/java/com/zerobase/weather/service/DiaryService.java
@@ -1,12 +1,25 @@
 package com.zerobase.weather.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
 public class DiaryService {
-    public void createDiary(LocalDate date,String text) {
 
+    @Value("${openweathermap-key}")
+    private String apiKey;
+
+    public void createDiary(LocalDate date,String text) {
+        getWeaterString();
+
+    }
+
+    private String getWeaterString() {
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
+        // 유효한 url인지 확인
+        System.out.println(apiUrl);
+        return "";
     }
 }
