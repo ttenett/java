cf311d3a71a4f8545879cd4675594c2fdb103ca9
ttenett <yeonjeongsdev@gmail.com>
2025-02-04 15:01:38 +0900

    feat: create kkpp

---
+++ java/project/kkpp/src/test/java/com/kkpp_food/kkpp/KkppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkpp_food.kkpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
